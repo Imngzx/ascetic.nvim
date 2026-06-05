0e39a31789c0ec8753aab6cffaf830e55a9858d7
Imngzx <117638365+Imngzx@users.noreply.github.com>
2026-06-05 13:16:31 +0800

    fix(code): removed old api

--- lua/ascetic/init.lua
+++ lua/ascetic/init.lua
@@ -11,12 +11,18 @@ local M = {}
 
 local api = vim.api
 local uv = vim.uv
-local v = vim.v
-local bo = vim.bo
-local keymap = vim.keymap
+local type = type
+local pcall = pcall
 local str_format = string.format
 local tbl_extend = vim.tbl_deep_extend
 local now = uv.now
+
+local nvim_create_user_command = api.nvim_create_user_command
+local nvim_get_vvar = api.nvim_get_vvar
+local nvim_get_option_value = api.nvim_get_option_value
+local keymap_set = vim.keymap.set
+local vim_notify = vim.notify
+local vim_deepcopy = vim.deepcopy
 
 ---@type AsceticConfig
 local default_config = {
@@ -27,13 +33,13 @@ local default_config = {
   smart_j_k = false,
   message = 'Hold it! Use Flash or motion keys (w, b, e) instead. Stop spamming `%s`!',
   notify = function(msg)
-    pcall(vim.notify, msg, 3, {
+    pcall(vim_notify, msg, 3, {
       title = 'Ascetic',
       id = 'ascetic_spam_blocker',
     })
   end,
 }
-local config = vim.deepcopy(default_config)
+local config = vim_deepcopy(default_config)
 
 --- enable
 function M.enable()
@@ -62,44 +68,46 @@ end
 function M.setup(opts)
   config = tbl_extend('force', config, opts or {})
 
-  local create_cmd = api.nvim_create_user_command
-  create_cmd('AsceticEnable', M.enable, { desc = 'Enable ascetic.nvim' })
-  create_cmd('AsceticDisable', M.disable, { desc = 'Disable ascetic.nvim' })
-  create_cmd('AsceticToggle', M.toggle, { desc = 'Toggle ascetic.nvim' })
+  nvim_create_user_command('AsceticEnable', M.enable, { desc = 'Enable ascetic.nvim' })
+  nvim_create_user_command('AsceticDisable', M.disable, { desc = 'Disable ascetic.nvim' })
+  nvim_create_user_command('AsceticToggle', M.toggle, { desc = 'Toggle ascetic.nvim' })
 
+  local num_keys = #config.keys
   local counts = {}
   local last_times = {}
+
+  for i = 1, num_keys do
+    counts[i] = 0
+    last_times[i] = 0
+  end
 
   local cfg_threshold = config.threshold
   local cfg_timeout = config.timeout
   local cfg_smart = config.smart_j_k
-  local set_map = keymap.set
+  local buf_opts = { buf = 0 }
 
-  for i = 1, #config.keys do
+  for i = 1, num_keys do
     local key = config.keys[i]
-    counts[key] = 0
-    last_times[key] = 0
-
     local is_jk = key == 'j' or key == 'k'
     local g_key = 'g' .. key
 
-    set_map({ 'n', 'x' }, key, function()
-      local c_count = v.count
+    keymap_set({ 'n', 'x' }, key, function()
+      local c_count = nvim_get_vvar('count')
       local exec_key = (cfg_smart and is_jk and c_count == 0) and g_key or key
 
       if not config.enabled then return exec_key end
       if c_count > 0 then
-        counts[key] = 0
+        counts[i] = 0
         return exec_key
       end
-      if bo.buftype ~= '' then return exec_key end
+      if nvim_get_option_value('buftype', buf_opts) ~= '' then return exec_key end
 
       local current_time = now()
-      local diff = current_time - last_times[key]
+      local diff = current_time - last_times[i]
 
-      local count = (diff > cfg_timeout) and 1 or (counts[key] + 1)
-      counts[key] = count
-      last_times[key] = current_time
+      local count = (diff > cfg_timeout) and 1 or (counts[i] + 1)
+      counts[i] = count
+      last_times[i] = current_time
 
       if count >= cfg_threshold then
         local msg_cfg = config.message
